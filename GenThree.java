f94f7cac225ce866858323c0d9b051c6f3068b66
Ella Balter <ellabalter@Ellas-MacBook-Pro.local>
2023-12-20 17:36:37 +0200

    HomeWork01

--- GenThree.java
+++ GenThree.java
@@ -1,9 +1,18 @@
-/*
- * Generates three random integers, each in a given range [a,b),
- * prints them, and then prints the minimal number that was generated.
- */
+
+
+
 public class GenThree {
-	public static void main(String[] args) {
-		// Put your code here	
-	}
+public static void main(String[] args) {
+
+	int a = Integer.parseInt(args[0]);
+	int b = Integer.parseInt(args[1]);
+
+	if (a > b) {
+		int temp = a;
+		a = b;
+		b = temp;
 }
+	System.out.println(((int)(Math.random()*(b - a )) + a));
+	System.out.println(((int)(Math.random()*(b - a )) + a));
+	System.out.println(((int)(Math.random()*(b - a )) + a));
+}}
